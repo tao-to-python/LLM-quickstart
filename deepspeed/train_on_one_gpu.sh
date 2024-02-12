862ec452f34a6a6df3506578dcdb767cc46f147e
tao-to-python <2285166171@qq.com>
2024-02-12 16:45:30 +0000

    处理gendata数据

--- deepspeed/train_on_one_gpu.sh
+++ deepspeed/train_on_one_gpu.sh
@@ -36,6 +36,20 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero2.json \
 --model_name_or_path t5-large \
+--per_device_train_batch_size 1 \
+--per_device_eval_batch_size 1 \
+--output_dir output_dir --overwrite_output_dir --fp16 \
+--do_train \
+--do_eval \
+--max_train_samples 500 --num_train_epochs 1 \
+--dataset_name wmt16 --dataset_config "ro-en" \
+--source_lang en --target_lang ro
+
+
+# DeepSpeed ZeRO-2 模式单 GPU 训练翻译模型（T5-Large）
+deepspeed --num_gpus=1 translation/run_translation.py \
+--deepspeed config/ds_config_zero2.json \
+--model_name_or_path t5-large \
 --per_device_train_batch_size 4 \
 --per_device_eval_batch_size 4 \
 --output_dir output_dir --overwrite_output_dir \
@@ -47,6 +61,15 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --fp16 \
 
 
+
+# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+deepspeed --num_gpus=1 translation/run_translation.py \
+--deepspeed config/ds_config_zero3.json \
+--model_name_or_path t5-Large --per_device_train_batch_size 1 \
+--output_dir output_dir --overwrite_output_dir --fp16 \
+--do_train --max_train_samples 500 --num_train_epochs 1 \
+--dataset_name wmt16 --dataset_config "ro-en" \
+--source_lang en --target_lang ro
 
 # DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
 deepspeed --num_gpus=1 translation/run_translation.py \
