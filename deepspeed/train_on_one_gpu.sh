c61899e68b60f05273c13992dae63a3063a40df6
tao-to-python <2285166171@qq.com>
2024-02-18 15:54:31 +0000

    提交第八周作业

--- deepspeed/train_on_one_gpu.sh
+++ deepspeed/train_on_one_gpu.sh
@@ -38,7 +38,7 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --model_name_or_path t5-large \
 --per_device_train_batch_size 1 \
 --per_device_eval_batch_size 1 \
---output_dir output_dir --overwrite_output_dir --fp16 \
+--output_dir output_dir --overwrite_output_dir  \
 --do_train \
 --do_eval \
 --max_train_samples 500 --num_train_epochs 1 \
@@ -58,11 +58,12 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
 --source_lang en --target_lang ro \
---fp16 \
+#--fp16 \
 
 
 
 # DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# 运行记录：显存占用3G，出现loss scale降低至1的情况
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero3.json \
 --model_name_or_path t5-Large --per_device_train_batch_size 1 \
@@ -72,10 +73,31 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --source_lang en --target_lang ro
 
 # DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# 运行记录：显存占用4.7G，训练正常
+deepspeed --num_gpus=1 translation/run_translation.py \
+--deepspeed config/ds_config_zero3.json \
+--model_name_or_path t5-Large --per_device_train_batch_size 1 \
+--output_dir output_dir --overwrite_output_dir  \
+--do_train --max_train_samples 500 --num_train_epochs 1 \
+--dataset_name wmt16 --dataset_config "ro-en" \
+--source_lang en --target_lang ro
+
+# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# 训练结果：exits with return code = -9
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero3.json \
 --model_name_or_path t5-3b --per_device_train_batch_size 1 \
 --output_dir output_dir --overwrite_output_dir --fp16 \
+--do_train --max_train_samples 500 --num_train_epochs 1 \
+--dataset_name wmt16 --dataset_config "ro-en" \
+--source_lang en --target_lang ro
+
+# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# 训练结果：cpu超过32G，gpu占用不到1G
+deepspeed --num_gpus=1 translation/run_translation.py \
+--deepspeed config/ds_config_zero3.json \
+--model_name_or_path t5-3b --per_device_train_batch_size 1 \
+--output_dir output_dir --overwrite_output_dir  \
 --do_train --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
 --source_lang en --target_lang ro
